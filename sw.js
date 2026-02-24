035b686beca74c8c397850d6592e697ed0bb546b
hasib61714 <hasib61714@users.noreply.github.com>
2026-02-24 21:43:15 +0000

    deploy: ebebb694191cab4e28aaad9a1ca369bc90040481

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // IMAP Service Worker – v1.9
-const CACHE_NAME = "imap-v15";
+const CACHE_NAME = "imap-v22";
 // Derive base path from SW location (works for both "/" and "/imap-bangladesh/")
 const BASE = new URL("./", self.location.href).href;
 const STATIC_ASSETS = [
